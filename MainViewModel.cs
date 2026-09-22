8cd219edb79810f162070924d67570c0ec5b5f21
mochinekoo <mochinekoo0113@gmail.com>
2026-09-23 03:16:36 +0900

    [add] ウインドウサイズを取得できるところまで追加しました

---
+++ MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+
+using Rect = WPF_WindowSizeChange.MainWindow.Rect;
+
+namespace WPF_WindowSizeChange {
+    internal class MainViewModel : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private MainWindow MainWindow { get; set; }
+
+        public MainViewModel(MainWindow mainWindow)
+        {
+            this.MainWindow = mainWindow;
+        }
+
+        public string TaskComboBox { get; 
+            set {
+                foreach (Process process in MainWindow.ProcessList)
+                {
+                    if (process.MainWindowHandle != IntPtr.Zero)
+                    {
+                        if (process.ProcessName == value)
+                        {
+
+                            if (MainWindow.GetWindowRect(process.MainWindowHandle, out Rect rect))
+                            {
+                                MainWindow.WidthBox.Text = (rect.Right - rect.Left).ToString();
+                                MainWindow.HeightBox.Text = (rect.Bottom - rect.Top).ToString();
+                            }
+
+                            
+                        }
+                    }
+                }
+                        
+            } 
+        }
+
+    }
+}
